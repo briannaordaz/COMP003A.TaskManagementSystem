1b3702b2720c427eedc8742daa29e4855e8bfe0c
briannaordaz <briannaordaz5@gmail.com>
2025-03-02 23:01:52 -0800

    added comment for Task class

--- TaskManagementSystem/Task.cs
+++ TaskManagementSystem/Task.cs
@@ -20,11 +20,12 @@ public abstract class Task
     /// <exception cref="ArgumentException"></exception>
     public string NameOfTask
     {
-        get { return _nameOfTask; }
+        get { return _nameOfTask; } //this will provide access to the private field "private string _nameOfTask;"
         set
         {
-            if (!string.IsNullOrWhiteSpace(value)) {_nameOfTask = value;}
-            else throw new ArgumentException("The task name cannot be empty.");
+            if (!string.IsNullOrWhiteSpace(value)) //this will check if the value is not empty,whitespace, or null 
+            {_nameOfTask = value;} //if it's not empty, whitespace, or null, then it will assign the value to the private field "private string _nameOfTask;"
+            else throw new ArgumentException("The task name cannot be empty."); //otherwise if its empty, null, or whitespace, then it will throw an exception saying "Task name cannot be empty."
             
         }
     }
@@ -33,12 +34,12 @@ public abstract class Task
     /// this is a public property for the due date of the task
     /// "DateTime?" will store the dates and times if the user decides to include it
     /// </summary>
-    public DateTime? DueDate
+    public DateTime? DueDate 
     {
-        get { return _dueDate; }
+        get { return _dueDate; } //this will provide access to the private field "private DateTime? _dueDate;"
         set
         {
-            _dueDate = value; 
+            _dueDate = value; //this will assign value to the private field "private DateTime? _dueDate;" when the property is being set
         }
         
     }
@@ -48,8 +49,8 @@ public abstract class Task
     /// </summary>
     public string Description
     {
-        get { return _descriptionOfTask; } 
-        set { _descriptionOfTask = value; }
+        get { return _descriptionOfTask; } //this will provide access to the private field "private string _descriptionOfTask ;"
+        set { _descriptionOfTask = value; } //this will assign value to the private field "private string _descriptionOfTask" when the property is being set
     }
 
     /// <summary>
